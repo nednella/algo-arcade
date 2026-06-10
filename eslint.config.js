5c873c46699c7815ac47585e9a72f1385b4f946d
nednella <119449811+nednella@users.noreply.github.com>
2026-06-10 14:47:29 +0100

    chore: add eslint unicorn plugin

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import ts from "typescript-eslint";
@@ -15,10 +16,26 @@ export default defineConfig([
       ts.configs.recommended,
       eslintPluginReactHooks.configs.flat.recommended,
       eslintPluginReactRefresh.configs.vite,
+      eslintPluginUnicorn.configs.recommended,
       eslintConfigPrettier // must be last
     ],
     languageOptions: {
       globals: globals.browser
+    },
+    rules: {
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            Props: true,
+            props: true,
+            Env: true,
+            env: true,
+            Utils: true,
+            utils: true
+          }
+        }
+      ]
     }
   }
 ]);
